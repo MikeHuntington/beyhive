55035f69ad8dd36a953d4be2655695fa4f1d5824
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2024-12-02 05:31:32 -0500

    cron fix

--- src/trigger/schedule.ts
+++ src/trigger/schedule.ts
@@ -3,7 +3,7 @@ import { schedules } from '@trigger.dev/sdk/v3'
 export const firstScheduledTask = schedules.task({
   id: 'first-scheduled-task',
   cron: {
-    pattern: '0 0 0/1 * *',
+    pattern: '0 0 * ? * * *',
     timezone: 'America/New_York',
   },
   run: async (payload) => {
